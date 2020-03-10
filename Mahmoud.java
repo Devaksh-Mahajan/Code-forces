e8b9789c1cee5bc4d32a21f113bf0f9595538fc5
Devaksh-Mahajan <43724577+Devaksh-Mahajan@users.noreply.github.com>
2020-03-10 19:56:10 +0530

    Mahmoud and Ehab and the even odd game

---
+++ Mahmoud.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class main
+{
+	public static void main(String[] args) {
+		Scanner c = new Scanner(System.in);
+		int n = c.nextInt();
+		for(int a=n;a>=n;a--)
+		{
+			if(a%2==0)
+			{
+				System.out.print("Mahmoud");
+			}
+			else
+			{
+				System.out.print("Ehab");
+			}
+		}
+	}
+}
