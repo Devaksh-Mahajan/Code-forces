a7981538b3d7d3f97fb14961a735b6083cd29ea1
Devaksh-Mahajan <43724577+Devaksh-Mahajan@users.noreply.github.com>
2020-03-20 16:45:18 +0530

    Elephant

---
+++ elephant.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main
+{
+	public static void main(String[] args) {
+		Scanner c = new Scanner(System.in);
+		int x = c.nextInt();
+		int count = 0;
+		int n;
+		n = (x+4)/5;
+		System.out.println(n);
+	}
+}
