d0f3b54aea5bab14b7f6703f2908fa7a35786bbb
Devaksh-Mahajan <43724577+Devaksh-Mahajan@users.noreply.github.com>
2020-03-23 13:19:55 +0530

    EhAb AnD gCd

---
+++ ehab.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class main
+{
+	public static void main(String[] args) {
+		Scanner c = new Scanner(System.in);
+		int t = c.nextInt();
+		int a=1;
+		int count=0;
+		int gcd;
+		for(int i=0;i<t;i++)
+		{
+			int x = c.nextInt(); // number whose sum we wanted
+			int k = x-1;
+			System.out.println(a + " " + k);
+
+		}
+
+	}
+}
