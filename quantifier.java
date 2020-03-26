10016c7609e5730c22ec5ed586473ee1e32c4c8a
Devaksh-Mahajan <43724577+Devaksh-Mahajan@users.noreply.github.com>
2020-03-27 01:56:58 +0530

    Quirky Quantifiers

---
+++ quantifier.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class main
+{
+	
+	public static void main(String[] args) {
+		Scanner c = new Scanner(System.in);
+	int a = c.nextInt();
+	if(a%2==0)
+	{
+		System.out.println("0");
+	}
+	else
+	{
+		System.out.println("1");
+	}
+	}
+}
