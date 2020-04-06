dd3dde576b20f95d8048ad51128665e8901e7263
Devaksh-Mahajan <43724577+Devaksh-Mahajan@users.noreply.github.com>
2020-04-06 20:37:07 +0530

    Domino Piling

---
+++ domino.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main
+{
+	public static void main(String[] args) {
+		Scanner c = new Scanner(System.in);
+		int m = c.nextInt();
+		int n = c.nextInt();
+		int ans = m*n;
+		int k = ans/2;
+		System.out.println(k);
+	}
+}
