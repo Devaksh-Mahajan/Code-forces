69d6c014cd9052d3b8254401815d5e223e972d51
Devaksh <43724577+Devaksh-Mahajan@users.noreply.github.com>
2020-06-24 01:30:34 +0530

    Ichihime and Triangle

---
+++ ichime.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			System.out.println(b + " " + c + " "  +c);
+		}
+	}
+}
